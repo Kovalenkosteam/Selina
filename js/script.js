6298a5849afe4f5b6767649c06a779993580c914
kovalenkosteam <kovalenkosteam@gmail.com>
2023-08-15 06:22:12 +0300

    v 1.5_click_header

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-const footerLink=document.querySelectorAll('.header_link');
+	const headerLinkParent = document.querySelector('.headerLinks');
+	const headerLink = document.querySelectorAll('.headerLink');
+	const mainContent = document.querySelectorAll('.mainContent');
 
+	function hideMainContent() {
+		mainContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('fade','show');
+		});
+		headerLink.forEach(item => { item.classList.remove('headerLinkActive') });
+	};
+	function showMainContent(i = 0) {
+		mainContent[i].classList.add('show','fade');
+		mainContent[i].classList.remove('hide');
+		headerLink[i].classList.add('headerLinkActive');
+	};
 
+	hideMainContent();
+	showMainContent();
 
-
+	headerLinkParent.addEventListener('click', (event) => {
+		const target = event.target;
+		if (target && target.classList.contains('headerLink')) {
+			headerLink.forEach((item, i) => {
+				if (target == item) {
+					hideMainContent();
+					showMainContent(i);
+				};
+			});
+		};
+	});
 
 
 
