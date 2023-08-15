5f76095c1de70ca556639b757de8226594ffc89f
kovalenkosteam <kovalenkosteam@gmail.com>
2023-08-15 20:53:01 +0300

    v 1.6_optimization_click

--- js/script.js
+++ js/script.js
@@ -1,103 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-	// const headerLinkParent = document.querySelector('.headerLinks');
-	// const headerLink = document.querySelectorAll('.headerLink');
-	// const mainContent = document.querySelectorAll('.mainContent');
-	// const roomLinksParent = document.querySelector('.roomLinks');
-	// const roomLink = document.querySelectorAll('.roomLink');
-	// const roomMainContent = document.querySelectorAll('.roomMainContent');
-
-	// function hideMainContent() {
-	// 	mainContent.forEach(item => {
-	// 		item.classList.add('hide');
-	// 		item.classList.remove('fade','show');
-	// 	});
-	// 	headerLink.forEach(item => { item.classList.remove('headerLinkActive') });
-	// };
-	// function showMainContent(i = 0) {
-	// 	mainContent[i].classList.add('show','fade');
-	// 	mainContent[i].classList.remove('hide');
-	// 	headerLink[i].classList.add('headerLinkActive');
-	// };
-
-	// hideMainContent();
-	// showMainContent();
-
-	// headerLinkParent.addEventListener('click', (event) => {
-	// 	const target = event.target;
-	// 	if (target && target.classList.contains('headerLink')) {
-	// 		headerLink.forEach((item, i) => {
-	// 			if (target == item) {
-	// 				hideMainContent();
-	// 				showMainContent(i);
-	// 			};
-	// 		});
-	// 	};
-	// });
-
 	const headerLinkParent = document.querySelector('.headerLinks');
 	const headerLink = document.querySelectorAll('.headerLink');
 	const mainContent = document.querySelectorAll('.mainContent');
+
 	const roomLinksParent = document.querySelector('.roomLinks');
 	const roomLink = document.querySelectorAll('.roomLink');
 	const roomMainContent = document.querySelectorAll('.roomMainContent');
 
-	function hideMainContent() {
-		mainContent.forEach(item => {
-			item.classList.add('hide');
-			item.classList.remove('fade','show');
-		});
-		headerLink.forEach(item => { item.classList.remove('headerLinkActive') });
-	};
-	function showMainContent(i = 0) {
-		mainContent[i].classList.add('show','fade');
-		mainContent[i].classList.remove('hide');
-		headerLink[i].classList.add('headerLinkActive');
-	};
 
-	function hideRoomMainContent() {
-		roomMainContent.forEach(item => {
-			item.classList.add('hide');
-			item.classList.remove('fade','show');
-		});
-		roomLink.forEach(item => { item.classList.remove('headerLinkActive') });
-	};
-	function showRoomMainContent(i = 1) {
-		roomMainContent[i].classList.add('show','fade');
-		roomMainContent[i].classList.remove('hide');
-		roomLink[i].classList.add('headerLinkActive');
-	};
+	function viewElements(link,parentLink,elementContent){
 
-	hideMainContent();
-	showMainContent();
-	hideRoomMainContent();
-	showRoomMainContent();
-
-	headerLinkParent.addEventListener('click', (event) => {
-		const target = event.target;
-		if (target  &&target.classList.contains('headerLink')) {
-			headerLink.forEach((item, i) => {
-				if (target == item) {
-					hideMainContent();
-					showMainContent(i);
-				};
+		function hideMainContent() {
+			elementContent.forEach(item => {
+				item.classList.add('hide');
+				item.classList.remove('fade','show');
 			});
+		link.forEach(item => { item.classList.remove('headerLinkActive') });
 		};
-	});
-
-	roomLinksParent.addEventListener('click', (event) => {
-		const targetRoom = event.target;
-		if (targetRoom  && targetRoom.classList.contains('roomLink')) {
-			roomLink.forEach((item, i) => {
-				if (targetRoom == item) {
-					hideRoomMainContent();
-					showRoomMainContent(i);
-				};
-			});
+		function showMainContent(i = 0) {
+			elementContent[i].classList.add('show','fade');
+			elementContent[i].classList.remove('hide');
+		link[i].classList.add('headerLinkActive');
 		};
-	});
+	
+		hideMainContent();
+		showMainContent();
+	
+		parentLink.addEventListener('click', (event) => {
+			const target = event.target;
+			link.forEach((item, i) => {
+					if (target == item) {
+						hideMainContent();
+						showMainContent(i);
+					};
+				});
+			
+		});
+	};
 
-
+	viewElements(headerLink,headerLinkParent,mainContent);
+	viewElements(roomLink,roomLinksParent,roomMainContent);
 
 
 
