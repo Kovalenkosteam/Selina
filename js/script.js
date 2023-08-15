04f59140e6d3a21ccea998831edb543b9ea493af
kovalenkosteam <kovalenkosteam@gmail.com>
2023-08-15 23:17:33 +0300

    v 1.7_attraction_start

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
 	const headerLinkParent = document.querySelector('.headerLinks');
@@ -9,38 +10,47 @@ document.addEventListener('DOMContentLoaded', () => {
 	const roomMainContent = document.querySelectorAll('.roomMainContent');
 
 
-	function viewElements(link,parentLink,elementContent){
+	function viewElements(link, parentLink, elementContent) {
 
 		function hideMainContent() {
 			elementContent.forEach(item => {
 				item.classList.add('hide');
-				item.classList.remove('fade','show');
+				item.classList.remove('fade', 'show');
 			});
-		link.forEach(item => { item.classList.remove('headerLinkActive') });
+			link.forEach(item => { item.classList.remove('headerLinkActive') });
 		};
 		function showMainContent(i = 0) {
-			elementContent[i].classList.add('show','fade');
+			elementContent[i].classList.add('show', 'fade');
 			elementContent[i].classList.remove('hide');
-		link[i].classList.add('headerLinkActive');
+			link[i].classList.add('headerLinkActive');
 		};
-	
+
 		hideMainContent();
 		showMainContent();
-	
+
 		parentLink.addEventListener('click', (event) => {
 			const target = event.target;
 			link.forEach((item, i) => {
-					if (target == item) {
-						hideMainContent();
-						showMainContent(i);
-					};
-				});
-			
+				if (target == item) {
+					hideMainContent();
+					showMainContent(i);
+				};
+			});
+
 		});
 	};
 
-	viewElements(headerLink,headerLinkParent,mainContent);
-	viewElements(roomLink,roomLinksParent,roomMainContent);
+	viewElements(headerLink, headerLinkParent, mainContent);
+	viewElements(roomLink, roomLinksParent, roomMainContent);
+
+
+
+	const hideImage = document.querySelectorAll('.attractionImage img');
+	for (let i = 1; i < hideImage.length; i++) {
+		hideImage[i].style.display = 'none';
+	}
+
+	
 
 
 
